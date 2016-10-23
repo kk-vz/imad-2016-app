9328ca98f5779d7bf0f1fbe1996b84b8dbcd2f4d
kk-vz <krishnakumar6039@gmail.com>
2016-10-23 09:54:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
 var x = document.getElementById("madi");
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft +10;
     x.style.marginLeft = marginLeft+"px";
