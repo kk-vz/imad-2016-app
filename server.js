baee64b66d0de71190f02ad139209deb6bd3eca4
kk-vz <krishnakumar6039@gmail.com>
2016-10-23 07:25:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,51 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article1 ={
+    title:"Article-1|KK",
+    heading:"Article One",
+    date:"Oct 24 2016",
+    content:`
+           <p>
+              Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.
+           </p>
+           <p>
+              Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.
+           </p>
+           <p>
+              Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.Article one.
+           </p>
+            `
+};
+function createHTML(data){
+    var htmlTemplate = `
+                        <html>
+                         <head>
+                           <title>${data.title}</title>
+                           <meta name="viewport" content="width-device-width" initial-scale="1">
+                           <link href="/ui/style.css" rel="stylesheet" />
+                           
+                         </head>
+                         <body>
+                          <div class = "container">
+                          <a href="/">Home</a>
+                          <hr>
+                          <div><h1>${data.heading}</h1></div>
+                          <br>      
+                          <div>${data.date}</div>
+                          <br>
+                          <div>
+                           ${data.content}
+                           </div>
+                          </div>
+                         </body>
+                        </html>
+
+                        `;
+    return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -22,7 +67,9 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/article-1', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','article-1.html'));
+  //res.sendFile(path.join(__dirname,'ui','article-1.html'));
+  res.send(createHTML());
+    
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
