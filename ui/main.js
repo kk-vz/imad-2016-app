1702f800525a2ed8a7d0270f48e36d12d6846490
kk-vz <krishnakumar6039@gmail.com>
2016-10-23 10:24:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,7 @@
 console.log('Loaded!');
 var x = document.getElementById("madi");
+var y = document.getElementById("yes");
+var z = document.getElementById("count");
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft +10;
@@ -8,4 +10,19 @@ function moveRight(){
 }
 x.onclick = function(){ 
     setInterval(moveRight,200);
+};
+yes.onclick = function(){
+    var req = new XMLHttpRequest();
+   
+    req.onreadystatechange = function(){
+        if(req.readystate == XMLHttpRequest.DONE){
+            if(req.status == 200){
+                var counter = req.responseText;
+                z.innerHTML = counter.toString();
+            }
+        }
+    }
+    
+    req.open('GET',"http://kk-vz.imad.hasura-app.io/counter",true);
+    req.send(null);
 };
