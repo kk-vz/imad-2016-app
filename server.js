6cb15b72ea96ca4d8b74ecf66959d0bfd67cad2b
kk-vz <krishnakumar6039@gmail.com>
2016-10-23 07:26:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/article-1', function (req, res) {
   //res.sendFile(path.join(__dirname,'ui','article-1.html'));
-  res.send(createHTML());
+  res.send(createHTML(article1));
     
 });
 
